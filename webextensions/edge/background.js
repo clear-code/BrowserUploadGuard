c3de7c6d40b25b59e5a046f6bcafa6b82f745f85
Takashi Hashida <hashida@clear-code.com>
2026-04-24 18:47:33 +0900

    Fix to back to a previous page when blocking

--- webextensions/edge/background.js
+++ webextensions/edge/background.js
@@ -1,8 +1,29 @@
 'use strict';
 
+/*
+ * When `{cancel: 1}` is used to block loading, Edge shows a warning page which
+ * indicates that loading is canceled by an add-on. To avoid it, move back to
+ * the previous page instead of blocking.
+ */
+const CANCEL_REQUEST = {redirectUrl:`data:text/html,${escape('<script type="application/javascript">history.back()</script>')}`};
+/*
+ *  Although even if we return `CANCEL_REQUEST` from `onBeforeRequest()` on a
+ *  sub-frame, `history.back()` will be performed against it's parent main
+ *  frame when there is no page to back in the sub-frame. As a result main
+ *  frame moves back to the previous page unexpectedly.
+ *  To avoid it, just move to blank page instead.
+ */
+const CANCEL_REQUEST_FOR_SUBFRAME = {redirectUrl:'about:blank'};
+
+function isBlocked(file) {
+  const blockedExtensions = [".exe", ".bat", ".cmd", ".js", ".vbs"];
+  return blockedExtensions.some(ext => file.endsWith(ext));
+}
+
 chrome.webRequest.onBeforeRequest.addListener(
   (details) => {
     if (!details.requestBody?.raw) return {};
+    const isMainFrame = (details.type == 'main_frame');
     for (const part of details.requestBody.raw) {
       if (part.file && isBlocked(part.file)) {
         if (details.tabId >= 0) {
@@ -11,7 +32,7 @@ chrome.webRequest.onBeforeRequest.addListener(
             path: part.file
           }).catch(() => {});  // 失敗しても無視
         }
-        return { cancel: true };
+        return isMainFrame ? CANCEL_REQUEST : CANCEL_REQUEST_FOR_SUBFRAME;
       }
     }
     return {};
